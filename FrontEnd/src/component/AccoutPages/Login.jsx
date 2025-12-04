ce4e80539fb84604cd9794bce3e855268963b5f6
Khawaja Zeeshan <khawajazeeshan03488944412@gmail.com>
2025-12-04 22:58:41 +0500

    MERN STACK

--- FrontEnd/src/component/AccoutPages/Login.jsx
+++ FrontEnd/src/component/AccoutPages/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
-import '../../styles/Login.css';
+import '../../Styles/Login.css';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
